36296dcc1d2eeb10213672b63777ddd0484b2f1e
Josh Mathiak <jmathiak4@gmail.com>
2021-12-28 12:04:47 -0600

    Added fetch method

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component, useState } from "react";
 
 function App() {
+  let xAPIKey = "d3f9e99f98c3ca67260b4e49369f2998";
+  const [weatherData, setWeatherData] = useState({});
+  const getWeather = () => {
+    fetch(
+      "http://api.openweathermap.org/data/2.5/forecast?zip=75035,us&appid=d3f9e99f98c3ca67260b4e49369f2998"
+    )
+      .then((response) => response.json())
+      .then((json) => setWeatherData(json));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getWeather}>Test</button>
+      <div>{weatherData.city.name}</div>
     </div>
   );
 }
